8cc786588514e27a7dcb2ffba2ec83fa10cd3b19
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-08-10 10:46:58 -0500

    updated contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,19 +9,19 @@ const socials = [
 		icon: <Twitter size={20} />,
 		href: "https://twitter.com/chronark_",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:kev.hernandezgarza@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "kev.hernandezgarza@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/Kevin-Hernandez-Garza",
 		label: "Github",
-		handle: "chronark",
+		handle: "Kevin-Hernandez-Garza",
 	},
 ];
 
